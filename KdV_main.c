906030838cc5bf38c157376ba73b949055b0da7c
Daniel Savage <djm87@fishercat.sr.unh.edu>
2014-04-28 09:45:17 -0400

    library linked for fftw.. rfftw not yet working

--- KdV_main.c
+++ KdV_main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <fftw.h>
+#include <rfftw.h>
 #include <complex.h>
 #include "Project1.h"
 
@@ -16,9 +17,9 @@ main(int argc, char **argv)
   const int n = 512; // number of points
   const int tmax = 0.1, nprint = 25, a = 9;// b = 16, c = 2;  
   double beg,end,dt,dx,dk,t_init,t_rk4;
-  fftw_complex in[n], out[n];
-  fftw_plan p; 
-  
+  fftw_real in[n], out[n];
+  rfftw_plan plan_forward; 
+  rfftw_plan plan_backward; 
    
   beg = WTime();
 
@@ -32,7 +33,9 @@ main(int argc, char **argv)
   struct vector *x = vector_create(n);
   struct vector *k = vector_create(n);
   struct vector *u = vector_create(n);
-
+  
+  plan_forward = fftw_create_plan(n,FFTW_FORWARD,FFTW_ESTIMATE);
+  plan_backward = fftw_create_plan(n,FFTW_BACKWARD,FFTW_ESTIMATE);
   FILE *f = fopen("Example1.asc","w");
 
   // initialize values 
@@ -45,22 +48,28 @@ main(int argc, char **argv)
      VEC(k, i) = -(n/2-1)*dk+dk*i;
    
    VEC(u,i) = 3*pow(a,2)*pow((1/cosh(0.5*a*VEC(x,i))),2); //checked and good... 
-   //printf("u(%2d) = %6.4f\t", i+1, VEC(u, i));
+   //printf("u(%2d) = %6.4g\n", i+1, VEC(u, i));
+   in[i].re = VEC(u,i);
+   in[i].im = 0.0; 
   }
+
  end = WTime();
   t_init = end-beg; 
 
   //printf("Number of Points = %d\n", n);
-  p = fftw_create_plan(n,FFTW_FORWARD,FFTW_ESTIMATE);
-  fftw_one(p,in,out); 
+  
+  rfftw_one( plan_forward,in,out );
+for (int i = 0; i < x->n; i++) {
+  in[i].im=out[i].im;
+  in[i].im=out[i].re; 
+}
+rfftw_one(plan_backward,in,out);
+  //fftw_one(p,in,out); 
   //assert(x->n==y->n);
 
-
-
- for (int i = 0; i < x->n; i++) {
-  printf("out= %6.4f\t",out[i].re);
-   }
-
+for (int i = 0; i < x->n; i++) {
+printf("i = %3d out= %12g %12g\n",i,out[i].re,out[i].im);
+ }
 
   // Solver RK4
   beg = WTime();
@@ -76,6 +85,8 @@ main(int argc, char **argv)
   vector_destroy(x);
   vector_destroy(k);
   vector_destroy(u);
-  fftw_destroy_plan(p); 
+  fftw_destroy_plan( plan_forward );
+  fftw_destroy_plan( plan_backward );  
+
   return 0;
 }
